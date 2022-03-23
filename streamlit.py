e3549cf2f89a0ef08ebcb82cbae1b97a2cbe817d
VASSAV <teamvassav@gmail.com>
2022-03-23 22:40:27 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,1 +1,23 @@
 
+import streamlit as st
+import pandas as pd
+
+st.write(""" # Welcome to VASSAV App ! # """)
+
+def data():
+    DATA = 'C:/Users/harisathwik/Desktop/CDAC.csv'
+    df = pd.read_csv(DATA)
+
+
+    df = pd.DataFrame(df, columns=["Employee", "Basic/Consolidated Pay"])
+
+    # plot the dataframe
+    st.line_chart(df)
+
+agree = st.checkbox('I agree to the terms and conditions')
+
+if agree:
+     st.write('Great!')
+     if st.button('Next'):
+         '''CDAC Salary'''
+         data()
